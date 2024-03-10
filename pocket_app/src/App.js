09134ebbe4bcdc291b51c2fd98ba6fa3e0940de6
timidgeek <9liveslinds@gmail.com>
2024-03-10 15:32:51 -0500

    task 3 updates

--- pocket_app/src/App.js
+++ pocket_app/src/App.js
@@ -1,6 +1,7 @@
 // functional imports
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 // component imports
 import Button from './components/general/Button';
@@ -18,10 +19,39 @@ function App() {
     { value: 'Oldest', label: 'Oldest' },
     { value: 'Highest Rated', label: 'Highest Rated' },
     { value: 'Lowest Rated', label: 'Lowest Rated' }
-  ]
+  ];
+
+  // define state variables
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState('');
+
+  useEffect(() => {
+    // function to execute on component mount
+    const fetchData = async () => {
+      const accessToken = localStorage.getItem('accessToken');
+      if (accessToken) {
+        try {
+          // send a post request to /api/auth/ with the authorization header set to Bearer <accessToken>
+          const response = await axios.post('/api/auth/', {}, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`
+            }
+          });
+          // on success, set isLoggedIn to true and userUsername to the username from the response object
+          setIsLoggedIn(true);
+          setUserUsername(response.data.username);
+        } catch (e) {
+          console.error('Error fetching user data:', e);
+        }
+      }
+    };
+
+    fetchData();
+  }, []); // empty dependency array ensures the effect runs only once on mount
 
   return (
     <div className="App">
+      {/* {isLoggedIn ? <Dashboard /> : <Authentication />} */}
       <Input />
       <SelectInput options={options}/>
       <Button label="Load More..." className="custom-button" />
